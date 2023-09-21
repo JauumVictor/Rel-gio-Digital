d31f62867a2c080b2e709fea8220fe9546ecd66a
JauumVictor <joaovictor9162@outlook.com>
2023-09-21 10:47:49 -0300

    Fixing script file path

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-var containers = document.querySelectorAll(".container");
-var [containerOne, containerTwo, containerThree] = containers;
+const containers = document.querySelectorAll(".container");
+const [containerOne, containerTwo, containerThree] = containers;
 
 /*
 Mapeando quais segmentos são ativados de acordo com seu numero, assim 
 podendo ativar ele usando um laço de iteração:
 */
-var segmentMap = {
+const segmentMap = {
   0: "012346",
   1: "34",
   2: "02356",
@@ -18,17 +18,22 @@ var segmentMap = {
   9: "123456"
 }
 
-
+/**
+ * 
+ * @param {Document} child 
+ * @param {number} number 
+ * @param {number} index 
+ */
 const activeteClass = (child, number, index) => {
 
   // Pegando os elementos filhos do display:
   child = child.children[index].children;
 
   // Removendo todos os elementos com a classe .active:
-[...child].forEach(x => x.classList.remove("active"));
+  [...child].forEach(x => x.classList.remove("active"));
 
   // Declarando o mapeamento do segmento:
-  var segment = segmentMap[number];
+  const segment = segmentMap[number];
 
   // Ativando os novos elementos ativos:
   for (const key of segment) {
@@ -36,27 +41,31 @@ const activeteClass = (child, number, index) => {
   }
 }
 
-
 // Formatação de numerais 9 --> 09:
+
+/**
+ * 
+ * @param {number} value 
+ * @param {number | string} totalWidth 
+ * @param {number | string} paddingChar 
+ * @returns 
+ */
 const leftPad = (value, totalWidth, paddingChar) => {
-  var length = totalWidth - value.toString().length + 1;
+  const length = totalWidth - value.toString().length + 1;
   return Array(length).join(paddingChar || '0') + value;
 }
 
-
 const setClock = () => {
-
-  var currentDate = new Date();
-  var seconds = leftPad(currentDate.getSeconds(), 2).split("");
-  var minutes = leftPad(currentDate.getMinutes(), 2).split("");
-  var hours = leftPad(currentDate.getHours(), 2).split("");
+  const currentDate = new Date();
+  const seconds = leftPad(currentDate.getSeconds(), 2).split("");
+  const minutes = leftPad(currentDate.getMinutes(), 2).split("");
+  const hours = leftPad(currentDate.getHours(), 2).split("");
 
   seconds.forEach((x, i) => activeteClass(containerThree, x, i));
   minutes.forEach((x, i) => activeteClass(containerTwo, x, i));
   hours.forEach((x, i) => activeteClass(containerOne, x, i));
 
 }
-
 
 setClock();
 setInterval(setClock, 1000);
